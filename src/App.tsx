c939f32cc1b50cb07f2f0a753fc93b860cbce341
Imane <imane.elfadil1-etu@etu.univh2c.ma>
2024-05-15 13:54:39 +0100

    html

--- src/App.tsx
+++ src/App.tsx
@@ -81,9 +81,9 @@ function Random() {
           </div>
         </div>
       </div>
-      <div className='flex justify-center z-0 mb-10 relative ml-[40%]'>
+      <div className='flex justify-center z-0 mb-10 relative ml-[20%]'>
       <TwitterShareButton url={"https://github.com/oebelus/numberFacts"} title={`Check out this interesting number fact:\n${slip}\n`}>
-        <button className='bg-white w-fit p-1 rounded-sm h-10'>
+        <button className='twitter bg-white w-fit p-1 rounded-sm h-10 hover:h-12'>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-twitter-x" viewBox="0 0 16 16">
             <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865l8.875 11.633Z"/>
           </svg>
